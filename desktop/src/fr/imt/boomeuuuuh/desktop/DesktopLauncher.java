f8b259bb0b9e820c8e612414776e9ad84679b89c
Romain Storaï <romain.storai62@gmail.com>
2022-02-09 10:58:56 +0100

    Created project

---
+++ desktop/src/fr/imt/boomeuuuuh/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.imt.boomeuuuuh.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.imt.boomeuuuuh.Boomeuuuuh;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Boomeuuuuh(), config);
+	}
+}
