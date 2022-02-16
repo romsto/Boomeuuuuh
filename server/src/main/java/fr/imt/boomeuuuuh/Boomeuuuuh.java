6ada12862ae4b5d431480a0ae50db0087fe28ddc
Romain Storaï <romain.storai62@gmail.com>
2022-02-16 08:47:09 +0100

    added test files and basic structure

---
+++ server/src/main/java/fr/imt/boomeuuuuh/Boomeuuuuh.java
@@ -0,0 +1,8 @@
+package fr.imt.boomeuuuuh;
+
+public class Boomeuuuuh {
+
+    public static void main(String[] args) {
+        // Server startup
+    }
+}
---
+++ server/src/test/java/fr/imt/test/Test.java
@@ -0,0 +1,11 @@
+package fr.imt.test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+    public void testDesTests() {
+        Assertions.assertEquals(1, 1);
+    }
+}
