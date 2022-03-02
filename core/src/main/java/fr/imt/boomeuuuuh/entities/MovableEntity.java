a61175aed83a949e6032716343f41f9d4ff63735
arnau <arnaud.pro.garry@gmail.com>
2022-03-02 11:03:27 +0100

    Le changement c m1tenant

--- core/src/main/java/fr/imt/boomeuuuuh/entities/MovableEntity.java
+++ core/src/main/java/fr/imt/boomeuuuuh/entities/MovableEntity.java
@@ -1,13 +1,9 @@
 package fr.imt.boomeuuuuh.entities;
 
 public abstract class MovableEntity extends Entity{
+
     public MovableEntity (int id){
         super(id);
     }
 
-    @Override
-    public void draw() {
-
-    }
-}
 }
