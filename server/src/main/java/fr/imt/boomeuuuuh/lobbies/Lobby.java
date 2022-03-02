829b7c0646bab6051894858bb10ebd9f0c6d51c4
BenoitFaureIMT <Dovahkiin2601>
2022-03-02 12:37:37 +0100

    Lobby - Added methods (to complete) which define it's function

--- server/src/main/java/fr/imt/boomeuuuuh/lobbies/Lobby.java
+++ server/src/main/java/fr/imt/boomeuuuuh/lobbies/Lobby.java
@@ -8,23 +8,36 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+//Class for a lobby
+//  -> Repertories players connected to it (and the subsequent game)
+//  -> Manages UDP communications with the players
+//  -> Manages information passed between palyers - eg. player skin that has to be relayed
+//  -> Manages Chat
+
 public class Lobby {
 
     private final LobbyConnection lobbyConnection;
     private final int udpPort;
+    private final int lobbyID;
+
+    //Player Variables
     private final Collection<Player> players;
     private Player owner;
     private String name;
 
-    public Lobby(int udpPort, String name, Player owner) throws SocketException {
+    public Lobby(int udpPort, int lobbyID, String name, Player owner) throws SocketException {
         this.udpPort = udpPort;
         this.lobbyConnection = new LobbyConnection(udpPort);
+        this.lobbyID = lobbyID;
+
         this.players = new ArrayList<>();
         this.owner = owner;
         this.name = name;
 
         addPlayer(owner);
     }
+
+    //-------------------------GET-------------------------
 
     public int getUdpPort() {
         return udpPort;
@@ -38,6 +51,8 @@ public class Lobby {
         return lobbyConnection;
     }
 
+    public int getLobbyID(){ return lobbyID; }
+
     public Player getOwner() {
         return owner;
     }
@@ -45,6 +60,9 @@ public class Lobby {
     public String getName() {
         return name;
     }
+
+    //-----------------------------------------------------
+    //------------------------SET--------------------------
 
     public void setName(String name) {
         this.name = name;
@@ -55,10 +73,73 @@ public class Lobby {
     }
 
     public void addPlayer(Player player) {
+        players.add(player);
 
+        //TODO : relay info to all client lobbys
     }
 
     public void removePlayer(Player player) {
+        players.remove(player);
 
+        //TODO : relay info to all client lobbys
     }
+    //-----------------------------------------------------
+    //--------------------Relay Methods--------------------
+    //  -> Used for communication
+
+    //CHAT (TCP)
+    //Relay chat from a player to all others
+    public void relayChat(List<String> chat, Player origin){
+        for (Player p:players) {
+            if (p != origin)
+                relayChat(chat, origin, p);
+        }
+    }
+    //Relay chat from a player to another
+    public void relayChat(List<String> chat, Player origin, Player target){
+        //TODO : do chat relay through players
+    }
+
+    //PLAYER INFO (TCP)
+    //Relay player specific info to all other players (player skin, name, stats)
+    public void relayPlayerInfo(Player origin){
+        for (Player p:players) {
+            if (p != origin)
+                relayPlayerInfo(origin, p);
+        }
+    }
+    //Relay player specific info to a target player
+    public void relayPlayerInfo(Player origin, Player target){
+        //TODO : Relay info through player
+    }
+
+    //PLAYER POSITIONS (TCP)
+    //Send position of all players
+    public void relayTCPPositions(List<PlayerPos> pos, Player target){
+        //TODO : Relay packet through player
+    }
+    //PLAYER POSITIONS (UDP)
+    //Send position updates to all players
+    public void relayUpdatedPositions(List<PlayerPos> pos){
+        for (Player p:players) {
+            relayUpdatedPositions(pos, p);
+        }
+    }
+    //Send position updates to one player
+    public void relayUpdatedPositions(List<PlayerPos> pos, Player target){
+        //TODO : Send packet through TCP
+    }
+
+    //BOMBS (TCP)
+    //  EXPLODE
+    public void sendExplosion(){}
+    //  PLACE
+    public void relayPlaceBomb(){}
+    //-----------------------------------------------------
+}
+
+class PlayerPos{ //TODO : Convert this shit to packets
+    public Player player;
+    public int posx;
+    public int posy;
 }
