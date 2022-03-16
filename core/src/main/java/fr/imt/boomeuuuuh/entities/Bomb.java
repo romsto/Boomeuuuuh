f3517ccf1e9a01edbc1e5fbed00b51c0d69d1211
arnau <arnaud.pro.garry@gmail.com>
2022-03-16 12:01:04 +0100

    Explosion comm macron

--- core/src/main/java/fr/imt/boomeuuuuh/entities/Bomb.java
+++ core/src/main/java/fr/imt/boomeuuuuh/entities/Bomb.java
@@ -2,6 +2,15 @@ package fr.imt.boomeuuuuh.entities;
 
 public class Bomb extends MovableEntity {
 
+    int power=1;
+
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+
+
 
     public Bomb(int id) {
         super(id);
--- core/src/main/java/fr/imt/boomeuuuuh/screens/MainMenuScreen.java
+++ core/src/main/java/fr/imt/boomeuuuuh/screens/MainMenuScreen.java
@@ -3,24 +3,35 @@ package fr.imt.boomeuuuuh.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import fr.imt.boomeuuuuh.Boomeuuuuh;
 import fr.imt.boomeuuuuh.MyGame;
+import fr.imt.boomeuuuuh.entities.BombeStandard;
+
+import java.nio.charset.StandardCharsets;
 
 public class MainMenuScreen implements Screen{
 
     private MyGame game;
     private Stage stage;
+    private BombeStandard st;
+    public SpriteBatch batch;
+    public float   temps;
 
     public MainMenuScreen(MyGame game){
         this.game = game;
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
+        batch= new SpriteBatch();
+        temps = 0.0F;
     }
 
     @Override
@@ -45,6 +56,8 @@ public class MainMenuScreen implements Screen{
         table.add(preferences).fillX().uniformX();
         table.row();
         table.add(exit).fillX().uniformX();
+        st = new BombeStandard(50);
+        st.setX(150);st.setY(150);st.setPower(6);
 
         // create button listeners
         exit.addListener(new ChangeListener() {
@@ -73,12 +86,19 @@ public class MainMenuScreen implements Screen{
     @Override
     public void render(float delta) {
         // clear the screen ready for next set of images to be drawn
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClearColor(0f, 0f, 1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // tell our stage to do actions and draw itself
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
+
+        temps += Gdx.graphics.getDeltaTime();
+        batch.begin();
+        st.draw(batch,temps );
+        //Game.draw(batch,temps);//
+        batch.end();
+
     }
 
     @Override
