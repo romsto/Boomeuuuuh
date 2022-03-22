99bee44721bf82654249335d5d7fc047ec0352ac
Romain Storaï <romain.storai62@gmail.com>
2022-03-22 15:06:48 +0100

    Updated the screen system to allow easier integration

--- core/src/main/java/fr/imt/boomeuuuuh/MyGame.java
+++ core/src/main/java/fr/imt/boomeuuuuh/MyGame.java
@@ -1,65 +1,49 @@
 package fr.imt.boomeuuuuh;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import fr.imt.boomeuuuuh.screens.*;
+import com.badlogic.gdx.Screen;
+import fr.imt.boomeuuuuh.screens.ScreenType;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MyGame extends Game {
 
-    private LoadingScreen loadingScreen;
-    private MainMenuScreen mainMenuScreen;
-    private OptionScreen optionScreen;
-    private PreferencesScreen preferencesScreen;
-    private LobbyScreen lobbyScreen;
-    private LobbySelectionScreen lobbySelectionScreen;
-    private PlayScreen playScreen;
-
-    public final static int MAINMENU = 0;
-    public final static int OPTIONS = 1;
-    public final static int PREFERENCES = 2;
-    public final static int LOBBY = 3;
-    public final static int LOBBYSELECTION = 4;
-    public final static int PLAY = 5;
+    private final Map<ScreenType, Screen> screens = new ConcurrentHashMap<>();
+    private ScreenType currentScreenType;
 
     @Override
     public void create() {
-        mainMenuScreen = new MainMenuScreen(this);
-        lobbyScreen = new LobbyScreen(this);
-        lobbySelectionScreen = new LobbySelectionScreen(this);
-        loadingScreen = new LoadingScreen(this);
-        playScreen = new PlayScreen(this);
-        optionScreen = new OptionScreen(this);
-        preferencesScreen = new PreferencesScreen(this);
-
-        setScreen(mainMenuScreen);
+        changeScreen(ScreenType.MAIN_MENU);
     }
 
-    public void changeScreen(int screen){
-        switch(screen){
-            case MAINMENU:
-                if(mainMenuScreen == null) mainMenuScreen = new MainMenuScreen(this);
-                break;
-            case PREFERENCES:
-                if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this);
-                this.setScreen(preferencesScreen);
-                break;
-            case OPTIONS:
-                if(optionScreen == null) optionScreen = new OptionScreen(this);
-                this.setScreen(optionScreen);
-                break;
-            case LOBBY:
-                if(lobbyScreen == null) lobbyScreen = new LobbyScreen(this);
-                this.setScreen(lobbyScreen);
-                break;
-            case LOBBYSELECTION:
-                if(lobbySelectionScreen == null) lobbySelectionScreen = new LobbySelectionScreen(this);
-                this.setScreen(lobbySelectionScreen);
-                break;
-            case PLAY:
-                if(playScreen == null) playScreen = new PlayScreen(this);
-                this.setScreen(playScreen);
-                break;
+    public ScreenType getCurrentScreenType() {
+        return currentScreenType;
+    }
+
+    public Screen getCurrentScreen() {
+        return getScreen(currentScreenType);
+    }
+
+    public Screen getScreen(ScreenType screenType) {
+        return screens.get(screenType);
+    }
+
+    public void changeScreen(ScreenType screenType) {
+        Screen screen = null;
+        if (screens.containsKey(screenType)) {
+            screen = screenType.instantiate(this);
+            screens.put(screenType, screen);
+        } else {
+            screen = screens.get(screenType);
+            if (screen == null) {
+                screen = screenType.instantiate(this);
+                screens.put(screenType, screen);
+            }
         }
+
+        currentScreenType = screenType;
+        setScreen(screen);
     }
 }
 
---
+++ core/src/main/java/fr/imt/boomeuuuuh/screens/ScreenType.java
@@ -0,0 +1,34 @@
+package fr.imt.boomeuuuuh.screens;
+
+import com.badlogic.gdx.Screen;
+import fr.imt.boomeuuuuh.MyGame;
+
+import java.lang.reflect.InvocationTargetException;
+
+public enum ScreenType {
+
+    LOADING(LoadingScreen.class),
+    LOBBY(LobbyScreen.class),
+    LOBBY_SELECTION(LobbySelectionScreen.class),
+    LOG_IN(LoginScreen.class),
+    MAIN_MENU(MainMenuScreen.class),
+    PREFERENCES(PreferencesScreen.class),
+    OPTION(OptionScreen.class),
+    PLAY(PlayScreen.class);
+
+    private final Class<? extends Screen> screenClass;
+
+    ScreenType(Class<? extends Screen> screenClass) {
+        this.screenClass = screenClass;
+    }
+
+    public Screen instantiate(MyGame myGame) {
+        Screen newInstance = null;
+        try {
+            newInstance = (Screen) screenClass.getDeclaredConstructors()[0].newInstance(myGame);
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return newInstance;
+    }
+}
