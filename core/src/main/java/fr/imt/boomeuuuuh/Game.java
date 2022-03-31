cb0d583f1d01179892da48b19b86f4f7d996ef37
Romain Storaï <romain.storai62@gmail.com>
2022-03-31 08:49:01 +0200

    minor update Game

--- core/src/main/java/fr/imt/boomeuuuuh/Game.java
+++ core/src/main/java/fr/imt/boomeuuuuh/Game.java
@@ -1,62 +1,84 @@
 package fr.imt.boomeuuuuh;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import fr.imt.boomeuuuuh.entities.Entity;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
 
-    public static List<Entity> entities = new ArrayList<>();
-    public int sizeX;
-    public int sizeY;
-    public int nbrJoueur;
-    public TiledMap map;
-    public  OrthogonalTiledMapRenderer renderer;
-    public SpriteBatch batch;
+    private static Game instance;
+
+    private final List<Entity> entities;
+    private final SpriteBatch batch;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
-
-
 
     public Game() {
         loadMap();
         batch = new SpriteBatch();
+
+        entities = new ArrayList<>();
     }
-    public void draw(float temps){
-        Gdx.gl.glClearColor(0,0,0,1);
+
+    /**
+     * Gets the Singleton Instance
+     *
+     * @return Game
+     */
+    public static Game getInstance() {
+        return instance;
+    }
+
+    public void draw(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         camera.update();
         renderer.setView(camera);
         renderer.render();
 
-        //for (Entity entity : entities) {
-        //    entity.draw(batch, temps);
-        //}
+        for (Entity entity : entities) {
+            entity.draw(batch, delta);
+        }
     }
-    public void loadMap(){
+
+    public void loadMap() {
         map = new TmxMapLoader().load("map/map1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
-        camera = new OrthographicCamera(480,480);
+        camera = new OrthographicCamera(480, 480);
         camera.position.x = 240;
         camera.position.y = 240;
-        Viewport viewport = new StretchViewport(480,480);
-
     }
 
-    public void dispose () {
+    public void dispose() {
         map.dispose();
         renderer.dispose();
     }
 
+    public void spawnEntity(Entity entity) {
+        entities.add(entity);
+        // TODO Create hitbox to make the entity react
+    }
 
+    public void removeEntity(Entity entity) {
+        // TODO Remove hitbox and kill entity
+        entities.remove(entity);
+    }
+
+    public Entity getEntity(int id) {
+        return entities.get(id);
+    }
+
+    public List<Entity> getEntities() {
+        return entities;
+    }
 }
