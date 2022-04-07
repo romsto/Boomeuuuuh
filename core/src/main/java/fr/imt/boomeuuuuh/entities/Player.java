8fb056d4b75079539f80daf31d0ac927b66b3985
Antoine Mathieu <antoine04.mathieu@gmail.com>
2022-04-07 11:46:13 +0200

    skin

--- core/src/main/java/fr/imt/boomeuuuuh/entities/Player.java
+++ core/src/main/java/fr/imt/boomeuuuuh/entities/Player.java
@@ -1,12 +1,16 @@
 package fr.imt.boomeuuuuh.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Shape;
 import com.badlogic.gdx.physics.box2d.World;
 import fr.imt.boomeuuuuh.utils.Location;
+import com.badlogic.gdx.graphics.g2d.Animation;
 
 public class Player extends MovableEntity {
 
@@ -15,6 +19,9 @@ public class Player extends MovableEntity {
     private String skin;
     private boolean referred = false;
     private float fontWidth;
+    private static final Texture skin1 = new Texture("skin/skinDown1.png");
+
+
 
     public Player(int id, Location location, World world) {
         super(id, location, world);
@@ -31,6 +38,9 @@ public class Player extends MovableEntity {
     public String getSkin() {
         return skin;
     }
+
+
+
 
     public void refer(String name, String skin) {
         this.referred = true;
@@ -59,6 +69,9 @@ public class Player extends MovableEntity {
         if (referred) {
             font.setColor(1f, (!isAffected ? 1f : 0), (!isAffected ? 1f : 0), 0.8f);
             font.draw(batch, name, getPixelX() + 16 - fontWidth / 2, getPixelY() + 45);
+            batch.draw(skin1,getPixelX(),getPixelY(),32,32);
+
+
         }
     }
 
