7c4bf76849e3eb7b36a4147d2efb4b7eef1c6229
tng13 <nicolautheo@gmail.com>
2022-04-07 10:32:15 +0200

    ajout des sons dans les assets son d'explosion des bombes

--- core/src/main/java/fr/imt/boomeuuuuh/screens/PlayScreen.java
+++ core/src/main/java/fr/imt/boomeuuuuh/screens/PlayScreen.java
@@ -3,6 +3,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import fr.imt.boomeuuuuh.Game;
 import fr.imt.boomeuuuuh.MyGame;
+import com.badlogic.gdx.audio.Sound;
 
 public class PlayScreen implements Screen {
 
@@ -11,6 +12,11 @@ public class PlayScreen implements Screen {
 
     // constructor to keep a reference to the main Game class
     public PlayScreen(MyGame game) {
+
+        Sound wavSound = Gdx.audio.newSound(Gdx.files.internal("Sounds/bomb.wav"));
+        wavSound.play();
+        wavSound.dispose();
+
         this.game = Game.getInstance();
         Gdx.input.setInputProcessor(this.game);
     }
