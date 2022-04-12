e8c7b36641b82c9220215dac8b1b0c3e67b7fc45
Antoine Mathieu <antoine04.mathieu@gmail.com>
2022-04-12 21:31:16 +0200

    powerup effects

--- core/src/main/java/fr/imt/boomeuuuuh/network/packets/server/PlayerInfoPacket.java
+++ core/src/main/java/fr/imt/boomeuuuuh/network/packets/server/PlayerInfoPacket.java
@@ -3,6 +3,7 @@ package fr.imt.boomeuuuuh.network.packets.server;
 import fr.imt.boomeuuuuh.Game;
 import fr.imt.boomeuuuuh.network.packets.Packet;
 import fr.imt.boomeuuuuh.network.packets.PacketType;
+import fr.imt.boomeuuuuh.utils.AssetsManager;
 
 public class PlayerInfoPacket extends Packet {
 
@@ -28,6 +29,9 @@ public class PlayerInfoPacket extends Packet {
         Game game = Game.getInstance();
         if (game == null)
             return;
+        if (game.player_bomb != maxBomb || game.player_speed != speed || game.player_bomb_power != power) {
+            AssetsManager.playSound("powerup");
+        }
         game.player_bomb = maxBomb;
         game.player_speed = speed;
         game.player_kills = kills;
